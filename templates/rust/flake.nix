3f8430c6b3087d286e7b380dcc87ce078e07366f
Marcus Whybrow <marcus@whybrow.uk>
2026-01-02 23:01:30 +0000

    chore: update rust template to use fenix for toolchain & update versions
    Was using rust-overlay provided by Mozilla but I preferred fenix because
    it was easier to optionally add in wasm32 build support.

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,30 +1,21 @@
 {
   description = "Rust project packaged using Naersk and rust-overlay";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
     naersk.url = "github:nix-community/naersk";
   };
-
   outputs = inputs: let 
-    pkgs = import inputs.nixpkgs { 
-      system = "x86_64-linux"; 
-      overlays = [ 
-        inputs.rust-overlay.overlays.default 
-      ];
-    };
-
-
-    # https://github.com/oxalica/rust-overlay#cheat-sheet-common-usage-of-rust-bin
-    rust-bin = pkgs.rust-bin.stable.latest.default; 
-
-    # https://github.com/nix-community/naersk
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    toolchain = inputs.fenix.packages.x86_64-linux.combine [
+      inputs.fenix.packages.x86_64-linux.latest.toolchain
+      # inputs.fenix.packages.x86_64-linux.wasm32-unknown-unknown.latest.rust-std
+    ];
     naersk = pkgs.callPackage inputs.naersk {
-      cargo = rust-bin;
-      rustc = rust-bin;
+      cargo = toolchain;
+      rustc = toolchain;
     };
-
     cargo = (pkgs.lib.importTOML ./Cargo.toml).package;
   in {
     packages.x86_64-linux.rust = naersk.buildPackage {
@@ -33,11 +24,11 @@
       src = pkgs.lib.cleanSource ./.;
     };
     packages.x86_64-linux.default = inputs.self.packages.x86_64-linux.rust;
-
     devShells.x86_64-linux.default = pkgs.mkShell {
       nativeBuildInputs = [ 
-        rust-bin
-        pkgs.bacon 
+        toolchain
+        pkgs.rust-analyzer
+        # pkgs.bacon 
       ];
     };
   };
